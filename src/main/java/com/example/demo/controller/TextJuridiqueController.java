8ea2fc1e663ee657bcd2a30e9bbb46142e012709
nourelhoudaas <sayahhouda06@gmail.com>
2024-10-06 16:07:09 +0100

    add

--- src/main/java/com/example/demo/controller/TextJuridiqueController.java
+++ src/main/java/com/example/demo/controller/TextJuridiqueController.java
@@ -11,7 +11,6 @@ import com.example.demo.service.TextJuridiqueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
