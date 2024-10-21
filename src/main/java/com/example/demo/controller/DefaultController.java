7a943361e78e15cff663808b520540a13f01a615
fadia-boumediene <fadiaboumediene11@gmail.com>
2024-10-21 09:57:58 +0100

    up

--- src/main/java/com/example/demo/controller/DefaultController.java
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -66,7 +66,7 @@ public class DefaultController {
         return "notAuthenticated/index";
     }
 
-    @GetMapping({"/login"})
+     @GetMapping({"/login"})
     public String login() {
         return "notAuthenticated/login";
     }
