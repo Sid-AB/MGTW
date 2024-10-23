eb3563e8cb0e5bff7a07b3c8cdd422952ceb7162
fadia-boumediene <fadiaboumediene11@gmail.com>
2024-10-23 15:04:20 +0100

    up

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
    @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authRequest) -> {
-         /*   ((AuthorizedUrl) authRequest.requestMatchers(new String[] { "/authenticated/**", "/user/**", "/tv/tvs",
+            ((AuthorizedUrl) authRequest.requestMatchers(new String[] { "/authenticated/**", "/user/**", "/tv/tvs",
                     "/tv/add", "/tv/save", "/tv/TVEdit/{id}", "/tv/update/{id}", "/textJuridique/textJuridiques",
                     "/textJuridique/add", "/textJuridique/save", "/radio/radios", "/radio/add", "/radio/save",
                     "/radio/RadioEdit/{id}", "/radio/update/{id}", "/presse/presses", "/presse/add", "/presse/save",
@@ -31,7 +31,7 @@ public class SecurityConfig {
                     "/caracterstique/radio/{id}/add", "/caracterstique/saveradio", "/caracterstique/save","/caracterstique/TvDelete/{id}","/caracterstique/RadioDelete/{id}",
                     "/agrument/agruments", "/agrument/add", "/agrument/save", "/ministers/add", "/ministers/Mins","/ministers/MinistersEdit/{id}","/ministers/update/{id}",
                     "/ministers/save", "/Direction/add", "/Direction/direction", "/Direction/save" ,"/sousDirection/add", "/sousDirection/sousdirection", "/sousDirection/save" }))
-                    .authenticated(); */
+                    .authenticated(); 
             ((AuthorizedUrl) authRequest.requestMatchers(new String[] { "/notAuthenticated/**", "/multimedia/**", "/",
                     "/index", "/tv", "/tv/public", "/tv/prive", "/tv/prive/{name}", "/tv/{id}", "/tv/lugo/{id}",
                     "/radio/public", "/radio/public/#national", "/radio/public/#local", "/radio/public/", "/radio/{id}",
@@ -44,7 +44,7 @@ public class SecurityConfig {
            http.formLogin((formLogin) -> {
             ((FormLoginConfigurer) formLogin.loginPage("/login").defaultSuccessUrl("/home", true)).permitAll();
         });
-        http.logout((logout) -> {
+         http.logout((logout) -> {
             logout.invalidateHttpSession(true).deleteCookies(new String[] { "JSESSIONID" }).clearAuthentication(true)
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout");
         }); 
