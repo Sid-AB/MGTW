c7bcbef33216f7b08a28e0a7c27e7f3757399e6e
fadia-boumediene <fadiaboumediene11@gmail.com>
2024-10-24 14:26:45 +0100

    up

--- src/main/java/com/example/demo/config/DataInit.java
+++ src/main/java/com/example/demo/config/DataInit.java
@@ -13,14 +13,14 @@ public class DataInit implements CommandLineRunner {
     private CategoriePresseRepository categoriePresseRepository;
     @Override
      public void run(String... args) throws Exception
-     {CategoriePress Cate1=new CategoriePress((long)1,"ثقافي","Culture","Cultural");
-     CategoriePress Cate2=new CategoriePress((long)2,"إقتصادي","Economie","Economic");
+     {CategoriePress Cate1=new CategoriePress((long)1,"ثقافي","Culturel","Cultural");
+     CategoriePress Cate2=new CategoriePress((long)2,"إقتصادي","Economique","Economic");
      CategoriePress Cate3=new CategoriePress((long)3,"عام","publique","public");
-     CategoriePress Cate4=new CategoriePress((long)4,"رياضي","sprot","sport");
+     CategoriePress Cate4=new CategoriePress((long)4,"رياضي","sport","sport");
      CategoriePress Cate5=new CategoriePress((long)5,"صحي","Santé","Hygienic");
-     CategoriePress Cate6=new CategoriePress((long)6,"سياحي","Touristique","Tourist");
-     CategoriePress Cate7=new CategoriePress((long)7,"فلاحي","Paysan","Peasant");
-     CategoriePress Cate8=new CategoriePress((long)8,"تكنولوجي","Technologique","Technology");
+     CategoriePress Cate6=new CategoriePress((long)6,"سياحي","Touristique","Touristic");
+     CategoriePress Cate7=new CategoriePress((long)7,"فلاحي","Agricole","Agricultural");
+     CategoriePress Cate8=new CategoriePress((long)8,"تكنولوجي","Technologique","Technological");
      CategoriePress Cate9=new CategoriePress((long)9,"سيارات","Automobile","Automobile");
         if(categoriePresseRepository.count() == 0)
         {
