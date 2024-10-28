3124edba56ca255084caea31a191f6edd6a0b974
nourelhoudaas <sayahhouda06@gmail.com>
2024-10-28 13:45:10 +0100

    update

--- src/main/java/com/example/demo/service/PresseService.java
+++ src/main/java/com/example/demo/service/PresseService.java
@@ -11,6 +11,7 @@ import com.example.demo.repository.MultimediaRepository;
 import com.example.demo.repository.PresseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
