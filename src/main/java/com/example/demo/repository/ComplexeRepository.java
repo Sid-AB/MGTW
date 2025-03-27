456febbe2affdfc553f2525f9c5071459db40fc0
= <=>
2025-03-27 13:25:26 +0100

    update

--- src/main/java/com/example/demo/repository/ComplexeRepository.java
+++ src/main/java/com/example/demo/repository/ComplexeRepository.java
@@ -3,7 +3,15 @@ package com.example.demo.repository;
 import com.example.demo.entities.Agrument;
 import com.example.demo.entities.Complexe;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
+
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +25,9 @@ public interface ComplexeRepository extends JpaRepository<Complexe, Long> {
     List<Complexe> findComplexeByAgrumentListAndType(List<Agrument> agruments, String string);
 
     List<Complexe> findComplexeByAgrumentListAndTypeAndName(List<Agrument> agruments, String string, String name);
+
+    @Modifying
+    @Transactional
+    @Query(value="DELETE FROM complexe_agrument WHERE complexe_agrument.complexe_id = :id",nativeQuery = true)
+    void deleteComplexeById(Long id);
 }
